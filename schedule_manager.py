80c35cd069a6735231db750e8a243c9878c82c77
TeaCat-Neya <TeaCat0416@gmail.com>
2025-08-29 21:59:57 +0900

    schedule first commit

--- schedule_manager.py
+++ schedule_manager.py
@@ -1,3 +1,5 @@
+from pydoc import describe
+
 import pymysql
 
 from pymysql.cursors import Cursor
@@ -12,15 +14,47 @@ def get_db_connection() -> pymysql.Connection:
 
 def add_schedule(cursor: Cursor):
     # Todo: 여기에 일정 추가 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    title = input('title:')
+    description = input('desc:')
+    start_datetime = input('start(yyyymmddhhmmss):')
+    end_datetime = input('end(yyyymmddhhmmss):')
+    sql = """INSERT INTO schedules(title, description, start_datetime, end_datetime)
+             VALUES (%s, %s, %s, %s)"""
+
+    cursor.execute(sql, (title, description, start_datetime, end_datetime))
+    print('일정이 추가됨.')
 
 def get_schedules(cursor: Cursor):
     # Todo: 여기에 일정 정보를 가져오는 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    sql = """SELECT id, title, description, start_datetime, is_completed FROM schedules"""
+
+    cursor.execute(sql)
+    schedules = cursor.fetchall()
+
+    if not schedules:
+        print('일정없음')
+        return
+
+    # 출력하기
+    for schedule in schedules:
+        id, title, description, start_datetime, is_completed = schedule
+        status = '완료' if is_completed else '진행중'
+
+        print(f'{id}: {title}')
+        print(f'진행상황: {status}')
+        print(f'일시:{start_datetime}')
+        if description:
+            print(f'내용:{description}')
+        print()
 
 def complete_schedule(cursor: Cursor):
     # Todo: 여기에 일정을 완료처리하는 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    id = input('완료 일정의 id: ') #id값 입력받기
+
+    sql = """UPDATE schedules SET is_completed = true WHERE id = %s"""
+
+    cursor.execute(sql, id)
+    print('일정완료')
 
 # 메뉴 나타내기
 def show_menu() -> str:
